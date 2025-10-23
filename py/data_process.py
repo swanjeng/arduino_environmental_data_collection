8d1fc3badc858e55adc40f951b3ca6e2063de0ee
Swan Jeng <swanjeng1205@gmail.com>
2025-10-23 18:36:42 +0800

    Add data processing and visualization for PM2.5
    This script processes data from a specified file and visualizes PM2.5 levels over time using matplotlib. It includes conditional shading based on maximum PM2.5 values.

---
+++ py/data_process.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from datetime import datetime
+data = {"date" : [],
+        "time" : [],
+        "t" : [],
+        "h" : [],
+        "pm1" : [],
+        "pm25" : [],
+        "pm10" : [],
+        "p" : []}
+with open("FILENAME", "r") as f: # change FILENAME to your file name
+    for line in f:
+        d1 = line.split()
+        
+        data["date"].append(d1[0])
+        data["time"].append(datetime.strptime(d1[1], "%H:%M:%S"))
+        data["t"].append(float(d1[2]))
+        data["h"].append(float(d1[3]))
+        data["pm1"].append(int(d1[4]))
+        data["pm25"].append(int(d1[5]))
+        data["pm10"].append(int(d1[6]))
+        data["p"].append(float(d1[7]))
+
+d = "pm25"
+al = 0.3
+Max = max(data[d])
+plt.plot(data["time"], data[d], color = "#000000")
+plt.fill_between(data["time"], [0] * len(data["date"]), [min(9, Max)] * len(data["date"]), color="#00FF00", alpha=al)
+if Max > 9: plt.fill_between(data["time"], [9] * len(data["date"]), [min(35.5, Max)] * len(data["date"]), color="#FFFF00", alpha=al)
+if Max > 35.5: plt.fill_between(data["time"], [35.5] * len(data["date"]), [min(55.5, Max)] * len(data["date"]), color="#FF8000", alpha=al)
+if Max > 55.5: plt.fill_between(data["time"], [55.5] * len(data["date"]), [min(125.5, Max)] * len(data["date"]), color="#FF0000", alpha=al)
+if Max > 125.5: plt.fill_between(data["time"], [125.5] * len(data["date"]), [min(225.5, Max)] * len(data["date"]), color="#8000FF", alpha=al)
+if Max > 225.5: plt.fill_between(data["time"], [225.5] * len(data["date"]), [max(data[d])] * len(data["date"]), color="#800000", alpha=al)
+plt.show()
