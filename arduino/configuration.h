cada7d4f9c845abbd430daad784e71e13ed13d51
Swan Jeng <swanjeng1205@gmail.com>
2025-10-24 14:59:49 +0800

    Update configuration.h

--- arduino/configuration.h
+++ arduino/configuration.h
@@ -3,9 +3,16 @@
 #define btn 3 // button pin
 #define fileName "LOG3.txt" // the file in SD card
 #define CS 10 // CS pin
-#define PMSserial Serial1
+
+// if your Arduino doesn't have Serial1, please uncomment the three lines below.
+// #include "softwareSerial.h"
+// softSerial = SoftwareSerial(rx, tx); // change rx and tx to the pins connected to PMS sensor
+// #define PMSserial softSerial
+#define PMSserial Serial1 // and comment this line.
+
 #define DHT22_pin A0
 #define led 4
 #define ssid "YOUR SSID"
 #define pass "YOUR PASSWORD"
-#define interval 30000 // collection interval
+#define interval 30000 // collection interval (ms)
+
