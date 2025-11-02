41c1370d5cda0a20747097e748ef29f0bb9f0e97
Swan Jeng <swanjeng1205@gmail.com>
2025-11-02 21:05:57 +0800

    Update and rename pm1_25_and_10_HOU.py to pm1_25_and_10_subplot.py

--- py/pm1_25_and_10_subplot.py
+++ py/pm1_25_and_10_subplot.py
@@ -1,15 +1,12 @@
-import matplotlib.pyplot as plt # 繪圖套件
-import matplotlib as mpl # 設定字體用
-import matplotlib.dates as mdates # 處理時間格式用
-from datetime import datetime # 將字串轉為時間格式的套件
-import glob # 批次尋找符合條件的檔案
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.dates as mdates
+from datetime import datetime
+import glob
 
-# 設定中文顯示
 plt.rcParams['font.sans-serif'] = ['SimSun']
 mpl.rcParams['axes.unicode_minus'] = False
 
-# 子圖繪製函式
-#          資料 dict, 子圖物件, 資料類型, 子圖位置, 線條顏色
 def subPlot(data, ax, type, p, col):
     time_formatter = mdates.DateFormatter('%H:%M:%S')
     ax[p - 1].xaxis.set_major_formatter(time_formatter)
@@ -17,14 +14,13 @@ def subPlot(data, ax, type, p, col):
     ax[p - 1].set_ylabel(type.upper())
     ax[p - 1].legend(loc = 'upper right')
 
-filenames = glob.glob(r'*HOU_CHEN_DU.txt')
+filenames = glob.glob(r'*abc.txt') # change abc to your file suffix for the same location
 
 types = ["pm1", "pm2.5", "pm10"]
 
 fig, ax = plt.subplots(3,1)
 
 for fileindex in range(len(filenames)):
-    if fileindex == 1: continue # 跳過資料偏差過大的檔案
     data = {"date" : [],
             "time" : [],
             "pm1" : [],
@@ -37,17 +33,17 @@ for fileindex in range(len(filenames)):
             dataInFile = line.split()
             
             t = datetime.strptime(dataInFile[1], "%H:%M:%S")
-            if t >= datetime.strptime("12:56:00", "%H:%M:%S") and t <= datetime.strptime("13:15:00", "%H:%M:%S"):
-                data["date"].append(dataInFile[0])
-                data["time"].append(datetime.strptime(dataInFile[1], "%H:%M:%S"))
+            data["date"].append(dataInFile[0])
+            data["time"].append(datetime.strptime(dataInFile[1], "%H:%M:%S"))
                 
-                for i in range(3):
-                    data[types[i]].append(int(dataInFile[i + 4]))
+            for i in range(3):
+                data[types[i]].append(int(dataInFile[i + 4]))
 
         for i in range(len(types)):
             subPlot(data, ax, types[i], i + 1, (0, fileindex / len(filenames), 0))
 
-plt.suptitle("PM1.0, PM2.5, and PM10 Levels Over Time at 好成嘟 Restaurant")
+plt.suptitle("PM1.0, PM2.5, and PM10 Levels Over Time at abc")
 plt.legend()
-# plt.tight_layout()
+# plt.tight_layout() # uncomment this if needed
+
 plt.show()
