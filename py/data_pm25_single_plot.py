2e411e78eb828570df47978b56239854004172bc
Swan Jeng <swanjeng1205@gmail.com>
2025-11-05 15:36:09 +0800

    Update data_pm25_single_plot.py

--- py/data_pm25_single_plot.py
+++ py/data_pm25_single_plot.py
@@ -1,3 +1,5 @@
+# For this program, when files are sorted by name, they should also be sorted by time.
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.font_manager import FontProperties as font
@@ -10,19 +12,18 @@ mpl.rcParams['axes.unicode_minus'] = False
 filenames = glob.glob(r'*abc.txt') # change abc to your file suffix for the same location
 
 for i in range(len(filenames)):
-    if i != 1:
-        data = {"date" : [],
-                "time" : [],
-                "pm25" : []}
-        with open(filenames[i], "r") as f:
-            for line in f:
-                d1 = line.split()
-                t = datetime.strptime(d1[1], "%H:%M:%S")
-                if t > datetime.strptime("12:56:00", "%H:%M:%S") and t < datetime.strptime("13:15:00", "%H:%M:%S"):
-                    data["time"].append(datetime.strptime(d1[1], "%H:%M:%S"))
-                    data["pm25"].append(int(d1[5]))
-            plt.plot(data["time"], data["pm25"], color = (0, i / len(filenames), 0), label = d1[0])
-            plt.legend()
+    data = {"date" : [],
+            "time" : [],
+            "pm25" : []}
+    with open(filenames[i], "r") as f:
+        for line in f:
+            d1 = line.split()
+            t = datetime.strptime(d1[1], "%H:%M:%S")
+            data["time"].append(datetime.strptime(d1[1], "%H:%M:%S"))
+            data["pm25"].append(int(d1[5]))
+    plt.plot(data["time"], data["pm25"], color = (0, i / len(filenames), 0), label = d1[0])
+    plt.legend()
 plt.title("PM2.5 Levels Over Time at abc")
 plt.show()
+
 
