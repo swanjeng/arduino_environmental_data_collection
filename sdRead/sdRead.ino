5007698d572cae433b02f876e665fe7739842055
Swan Jeng <swanjeng1205@gmail.com>
2025-11-18 09:03:29 +0800

    Update sdRead.ino

--- sdRead/sdRead.ino
+++ sdRead/sdRead.ino
@@ -1,13 +1,17 @@
 #include "SD.h"
 #include "SPI.h"
+#define FILENAME "your file name.txt"
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   SD.begin(10);
+
+  // Wait for the Python program to be ready 
   while (Serial.available() <= 0);
   byte dummy = Serial.read();
-  File f = SD.open("LOG3.txt");
+
+  // Send the content of the file to the Python program
+  File f = SD.open(FILENAME);
   if (f) {
     while (f.available()) {
       Serial.write(f.read());
@@ -15,18 +19,20 @@ void setup() {
     Serial.flush();
   }
   Serial.println();
-  Serial.println("A");
+  Serial.println("A"); // EOF signal
   f.close();
+
+  // Wait for the Python program to send "clear file" signal
   while (Serial.available() <= 0);
   dummy = Serial.read();
-  SD.remove("LOG3.txt");
-  File f1 = SD.open("LOG3.txt", FILE_WRITE);
+
+  // clear the file
+  SD.remove(FILENAME);
+  File f1 = SD.open(FILENAME, FILE_WRITE);
   f1.close();
   delay(1000);
   Serial.println("done");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
 }
