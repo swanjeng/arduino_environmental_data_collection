1e0177fd0912232a073da87fc5b6a36ecb14bd90
Swan Jeng <swanjeng1205@gmail.com>
2026-02-01 16:26:56 +0800

    Move things back to the new directory

---
+++ environmentDataCollection/configuration.h
@@ -0,0 +1,4 @@
+#define fileName "LOG3.txt" // the file in SD card
+#define ssid "SSID"
+#define pass "PASS"
+#define interval 30000 // collection interval
---
+++ environmentDataCollection/sensorValue.h
@@ -0,0 +1,47 @@
+#include <PMS.h>
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define PMSserial Serial1
+#define DHT22_pin A0
+
+Adafruit_BME280 bme;
+DHT dht(DHT22_pin, DHT22); 
+PMS pms(PMSserial);
+PMS::DATA data;
+
+void initSensors() {
+  PMSserial.begin(9600);
+  dht.begin();
+  bme.begin(0x76, &Wire);
+  pms.passiveMode();
+  pms.wakeUp();
+}
+
+String dhtdata() {
+  delay(10);
+  float t = dht.readTemperature();
+  delay(10);
+  float h = dht.readHumidity();
+  return String(t) + " " + String(h);
+}
+
+String pmsdata() {
+  String str = "";
+  pms.requestRead();
+  if (pms.readUntil(data)) {
+    str += String(data.PM_AE_UG_1_0);
+    str += " ";
+    str += String(data.PM_AE_UG_2_5);
+    str += " ";
+    str += String(data.PM_AE_UG_10_0);
+  } else str = "error error error";
+  return str;
+}
+
+String bmedata() {
+  return String(bme.readPressure() / 100.0);
+}
